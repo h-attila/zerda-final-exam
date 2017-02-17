7c522f7daa4afd3702bb282ffd0b5c284e61c8bf
h-attila <email.atinak@gmail.com>
2017-02-17 09:34:08 +0100

    for ok

--- exam-attila.js
+++ exam-attila.js
@@ -1,1 +1,16 @@
 // Attila exam
+
+// Create a function that takes a list of strings and a letter and returns a string where the strings are joined with the given letter like: ["ab", "cde", "fg"] and " " becomes "ab cde fg"
+
+let stringList = ['alma', 'korte', 'barack', 'szilva'];
+let spacer = '_';
+
+function stringJoiner (string, letter) {
+  let result = '';
+  for (i=0; i < 4; i++) {
+    result += string[i] + letter;
+  };
+  return result;
+};
+
+console.log(stringJoiner(stringList, letter));
