3e0d978ba75a6da1473f65be04af65ee8bf3f27d
h-attila <email.atinak@gmail.com>
2017-02-17 09:58:24 +0100

    solution3 ok

--- exam-attila.js
+++ exam-attila.js
@@ -1,11 +1,13 @@
-// Attila exam
+// Attila exam #16
 
 // Create a function that takes a list of strings and a letter and returns a string where the strings are joined with the given letter like: ["ab", "cde", "fg"] and " " becomes "ab cde fg"
 
 let stringList = ['alma', 'korte', 'barack', 'szilva'];
 let spacer = '_';
 
+// ÷÷÷÷÷÷÷÷÷÷÷÷÷
 // SOLUTION #1
+// ÷÷÷÷÷÷÷÷÷÷÷÷÷
 
 function stringJoiner (list, letter) {
   let result = '';
@@ -19,9 +21,23 @@ function stringJoiner (list, letter) {
   return result;
 };
 
+// ÷÷÷÷÷÷÷÷÷÷÷÷÷
 // SOLUTION #2
+// ÷÷÷÷÷÷÷÷÷÷÷÷÷
 
 function stringJoiner_2 (list, letter) {
+  let result = '';
+  for (i=0; i < list.length; i++) {
+    (i < list.length-1) ? result += list[i] + letter : result += list[i];
+    }
+  return result;
+};
+
+// ÷÷÷÷÷÷÷÷÷÷÷÷÷
+// SOLUTION #3
+// ÷÷÷÷÷÷÷÷÷÷÷÷÷
+
+function stringJoiner_3 (list, letter) {
   let result = '';
   for (i=0; i < list.length-1; i++) {
       result += list[i] + letter;
@@ -29,5 +45,7 @@ function stringJoiner_2 (list, letter) {
   return result + list[list.length-1];
 };
 
+
 console.log(stringJoiner(stringList, spacer));
 console.log(stringJoiner_2(stringList, spacer));
+console.log(stringJoiner_3(stringList, spacer));
